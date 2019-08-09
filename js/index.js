da779432569e4d0a16dfcb862ce4ea159c249ab4
sneiksus <34603663+sneiksus@users.noreply.github.com>
2019-08-09 10:54:02 +0300

    Delete index.js

--- js/index.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-    let canvasTrouble=$("#canvasTrouble");
-    //let shot=$("#shot");
-    canvasTrouble.hide();
-    $("#shot").on("click",function(){
-        canvasTrouble.show();
-    });
-})
